602fa73e805f1c3f5d703e3818998372e1439b8a
DESKTOP-OQQCHD6\admin <xc>
2025-04-11 17:00:54 +0700

    sda

--- CRUDExample/Program.cs
+++ CRUDExample/Program.cs
@@ -5,53 +5,72 @@ using Entities;
 using RepositoryContracts;
 using Repositories;
 using Serilog;
-var builder = WebApplication.CreateBuilder(args);
-builder.Host.UseSerilog((HostBuilderContext context,IServiceProvider services, LoggerConfiguration loggerConfiguration) =>
-{
-    loggerConfiguration.ReadFrom.Configuration(context.Configuration).ReadFrom.Services(services);
-});
-//loggin
-builder.Host.ConfigureLogging(loggingProvider =>
-{
-    loggingProvider.ClearProviders();
-    loggingProvider.AddConsole();
-    loggingProvider.AddDebug();
-    loggingProvider.AddEventLog();
-});
-builder.Services.AddControllersWithViews();
+using CRUDExample.Filters.ActionFilters;
 
-//add services into Ioc container
+var builder = WebApplication.CreateBuilder(args);
+
+//Serilog
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider services, LoggerConfiguration loggerConfiguration) => {
+
+    loggerConfiguration
+    .ReadFrom.Configuration(context.Configuration) //read configuration settings from built-in IConfiguration
+    .ReadFrom.Services(services); //read out current app's services and make them available to serilog
+});
+
+builder.Services.AddTransient<ResponseHeaderActionFilter>();
+
+//it adds controllers and views as services
+builder.Services.AddControllersWithViews(options => {
+    //options.Filters.Add<ResponseHeaderActionFilter>(5);
+
+    var logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<ResponseHeaderActionFilter>>();
+
+    options.Filters.Add(new ResponseHeaderActionFilter(logger)
+    {
+        Key = "My-Key-From-Global",
+        Value = "My-Value-From-Global",
+        Order = 2
+    });
+});
+
+//add services into IoC container
 builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
 builder.Services.AddScoped<IPersonsRepository, PersonsRepository>();
+
 builder.Services.AddScoped<ICountriesService, CountriesService>();
 builder.Services.AddScoped<IPersonsService, PersonsService>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
- { 
-     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); 
- });
-//Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PersonsDatabase;Integrated Security=True;
-//Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False
-builder.Services.AddHttpLogging(options => {
-    options.LoggingFields = 
-    Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestProperties | Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.ResponsePropertiesAndHeaders;
-
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddTransient<PersonsListActionFilter>();
+
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestProperties | Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.ResponsePropertiesAndHeaders;
+});
+
 var app = builder.Build();
 
 app.UseSerilogRequestLogging();
+
+//create application pipeline
 if (builder.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
+
 app.UseHttpLogging();
-//app.Logger.LogDebug("debug-message");
-//app.Logger.LogInformation("information-message");
-//app.Logger.LogWarning("warning-message");
-//app.Logger.LogError("error-message");
-//app.Logger.LogCritical("critical-message");
+
+//if (builder.Environment.IsEnvironment("Test") == false)
+//    Rotativa.AspNetCore.RotativaConfiguration.Setup("wwwroot", wkhtmltopdfRelativePath: "Rotativa");
 
 app.UseStaticFiles();
 app.UseRouting();
 app.MapControllers();
+
 app.Run();
+
+public partial class Program { } //make the auto-generated Program accessible programmatically
